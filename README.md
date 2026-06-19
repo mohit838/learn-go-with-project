4ee24b1b014b5cb5c205b7a1d1f37c7b5aedc8b1
Mohit <mohitul449@gmail.com>
2026-06-19 20:26:30 +0600

    feat: auth service base is written for test and other base file with minimal instructions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Learn Golang With Project
+# Expense Task Tracker Microservices POC
 
-- Learn golang with project
+- Expense Task Tracker is a microservices POC project that demonstrates the use of Go, Docker, and PostgreSQL to build a simple expense tracking application. The project is structured into multiple services, each responsible for a specific functionality.
---
+++ services/auth/cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/mohit838/learn-go-with-project/internal/config"
+)
+
+func main() {
+	// This is the Auth Service API
+	fmt.Println("This is the Auth Service API")
+
+	// Load environment variables
+	cfg, err := config.LoadConfig("./.env")
+	if err != nil {
+		log.Println("Error loading config:", err)
+		return
+	}
+
+	fmt.Println("App Name:", cfg.AppName)
+	fmt.Println("App Env:", cfg.AppEnv)
+	fmt.Println("App Port:", cfg.AppPort)
+	fmt.Println("Debug Mode:", cfg.AppDebug)
+
+	// Initialize database connection (later)
+	// Initialize Redis connection (later)
+
+	// Set up routes and handlers
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Auth Service API is running!"))
+	})
+
+	// Start the server
+	appPort := cfg.AppPort
+	if appPort == "" {
+		log.Fatal("Port is not specified")
+	}
+
+	fmt.Printf("Starting server on port %s...\n", appPort)
+	if err := http.ListenAndServe(":"+appPort, r); err != nil {
+		log.Fatal("Server failed:", err)
+	}
+}
