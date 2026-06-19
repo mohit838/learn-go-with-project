20b942052142c5128e33b1bb98f995c9ff7a3e15
Mohit <mohitul449@gmail.com>
2026-06-19 22:24:02 +0600

    feat: expense tracker services added new db conn

---
+++ services/expense-tracker/internal/router/router.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"database/sql"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func NewRouter(db *sql.DB) http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.ClientIPFromRemoteAddr)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Expense Tracker Service API is running!"))
+	})
+
+	return r
+}
